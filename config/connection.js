c4f45c426c6bb9cb3344beea152ed150e6396baf
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-11-04 16:26:13 -0500

    Added Heroku process env for MLab MongoDB URI

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const MONGOOSE_URI = process.env.MONGOOSE_URI || "mongodb://localhost/articles_db";
 
-module.exports = mongoose.connect("mongodb://localhost/articles_db", {
+
+module.exports = mongoose.connect(MONGOOSE_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
